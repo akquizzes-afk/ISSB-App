06d50f9256a907d9291ef22295a29bf92d56a927
akquizzes-afk <akquizzes@gmail.com>
2025-11-16 16:36:50 +0500

    update native-back.js

--- js/native-back.js
+++ js/native-back.js
@@ -1,17 +1,22 @@
-// This code waits for the Capacitor API to be ready
-document.addEventListener('deviceready', () => {
-    // Get the Capacitor 'App' plugin
-    const { App } = window.Capacitor.Plugins;
+// This script runs *after* capacitor.js has been loaded in the HTML.
+// We do not need to wait for any event.
 
-    // Add a listener for the native 'backButton' event
-    App.addListener('backButton', () => {
-        // Check if there is any history to go back to
-        if (window.history.length > 1) {
-            // If yes, just go back one page
-            window.history.back();
-        } else {
-            // If no history (we are on index.html), then exit the app
-            App.exitApp();
-        }
-    });
+// Get the App plugin from the Capacitor global object.
+const { App } = window.Capacitor.Plugins;
+
+// Add a listener for the native 'backButton' event
+App.addListener('backButton', () => {
+    
+    // Check if the browser history has any pages to go back to.
+    // 'window.history.length > 1' means we are not on the first page (index.html).
+    if (window.history.length > 1) {
+        
+        // If we are on 'wat-test.html', this will go back to 'index.html'.
+        window.history.back();
+        
+    } else {
+        
+        // If we are on 'index.html' (history length is 1), exit the app.
+        App.exitApp();
+    }
 });
